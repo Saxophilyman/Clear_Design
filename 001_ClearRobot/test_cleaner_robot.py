61dafaa1ce824b7b265b3b95f26512b57f66e0d5
Saxophilyman <zd_@mail.ru>
2026-04-12 23:08:43 +0400

    step-003

--- 001_ClearRobot/test_cleaner_robot.py
+++ 001_ClearRobot/test_cleaner_robot.py
@@ -6,6 +6,12 @@ def test_move_from_start():
     result = robot.execute('move 100')
     assert result == 'POS 100,0'
 
+def test_move_another_from_start():
+    robot = RobotController()
+
+    result = robot.execute('move 50')
+
+    assert result == 'POS 50,0'
     # -- либо текущую позицию: POS x,y (команда move),
     # -- либо текущий угол поворота в градусах: ANGLE a (команда turn),
     # -- либо текущее состояние устройства очистки (одно из трёх): STATE water/soap/brush (команда set),
