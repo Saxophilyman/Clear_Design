049978717a9f9bedad5c6adedd5d57cc55cedc47
Saxophilyman <zd_@mail.ru>
2026-04-12 23:30:29 +0400

    step-007 (зелёные)

--- 001_ClearRobot/cleaner_robot.py
+++ 001_ClearRobot/cleaner_robot.py
@@ -1,7 +1,12 @@
 
 
 class RobotController:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
     def execute(self, command: str) -> str:
         parts = command.split()
-        distance = parts[1]
-        return f'POS {distance},0'
+        distance = int(parts[1])
+        self.x += distance
+        return f'POS {self.x},{self.y}'
