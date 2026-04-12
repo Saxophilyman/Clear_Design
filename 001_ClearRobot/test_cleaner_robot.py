7e25da77ea67657bf239302d329368f000b9632b
Saxophilyman <zd_@mail.ru>
2026-04-13 00:51:21 +0400

    step-017 (все set)

--- 001_ClearRobot/test_cleaner_robot.py
+++ 001_ClearRobot/test_cleaner_robot.py
@@ -2,19 +2,16 @@ from cleaner_robot import RobotController
 # Move (двигаться вперед на заданное число метров)
 def test_move_from_start():
     robot = RobotController()
-
     result = robot.execute('move 100')
     assert result == 'POS 100,0'
 
 def test_move_another_from_start():
     robot = RobotController()
-
     result = robot.execute('move 50')
     assert result == 'POS 50,0'
 
 def test_move_twice_plus_position():
     robot = RobotController()
-
     robot.execute('move 100')
     result = robot.execute('move 50')
     assert result == 'POS 150,0'
@@ -22,43 +19,42 @@ def test_move_twice_plus_position():
 # Turn (поворот на месте на заданный угол в градусах)
 def test_turn_changes_angle():
     robot = RobotController()
-
     result = robot.execute('turn -90')
     assert result == 'ANGLE -90'
 
 def test_move_after_turn_changes():
     robot = RobotController()
-
     robot.execute('turn 90')
     result = robot.execute('move 50')
-
     assert result == 'POS 0,50'
 
 def test_move_after_negative_turn_changes():
     robot = RobotController()
-
     robot.execute('turn -90')
     result = robot.execute('move 50')
-
     assert result == 'POS 0,-50'
 
-
-def test_set_cleaning_mode():
+# Set (выбрать одно из трёх устройств очистки)
+def test_set_cleaning_mode_soap():
     robot = RobotController()
-
     result = robot.execute('set soap')
     assert result == 'STATE soap'
 
+def test_set_cleaning_mode_water():
+    robot = RobotController()
+    result = robot.execute('set water')
+    assert result == 'STATE water'
+
+def test_set_cleaning_mode_brush():
+    robot = RobotController()
+    result = robot.execute('set brush')
+    assert result == 'STATE brush'
 
     # -- либо текущую позицию: POS x,y (команда move),
     # -- либо текущий угол поворота в градусах: ANGLE a (команда turn),
-
-# Команда set может принимать один из трёх параметров
-# water (полив водой; исходное по умолчанию),
-# soap (мыльная пена),
-# brush (метла).
     # -- либо текущее состояние устройства очистки (одно из трёх):
-        # STATE water/soap/brush (команда set),
+        # STATE water(по умолчанию)/soap/brush (команда set),
+
     # -- либо начало работы с текущим состоянием устройства очистки: START WITH water/soap/brush (команда start),
     # -- либо прекращение работы: STOP (команда stop).
 
