190c8c1f5bb232e9f2094ffb51ce5ab540b5293f
Saxophilyman <zd_@mail.ru>
2026-04-29 00:44:56 +0400

    006. with Example

---
+++ 006_z_High_Level/cleaner_api.py
@@ -0,0 +1,99 @@
+import pure_robot
+
+class RobotCommand:
+
+    def __init__(self, input_state, command):
+        self.state = input_state
+        self.command = command
+
+def transfer_to_cleaner(message):
+    print (message)
+
+##########
+
+import queue
+import threading
+
+import pure_robot
+
+
+input_queue = queue.Queue()
+output_queue = queue.Queue()
+
+
+class ThreadRobo(threading.Thread):
+    def __init__(self, in_queue, out_queue):
+        super().__init__()
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+
+    def run(self):
+        while True:
+            command = self.in_queue.get()
+
+            try:
+                cleaner_state = command.state
+                cmd = command.command.split()
+
+                if cmd[0] == "move":
+                    cleaner_state = pure_robot.move(
+                        transfer_to_cleaner,
+                        int(cmd[1]),
+                        cleaner_state
+                    )
+
+                elif cmd[0] == "turn":
+                    cleaner_state = pure_robot.turn(
+                        transfer_to_cleaner,
+                        int(cmd[1]),
+                        cleaner_state
+                    )
+
+                elif cmd[0] == "set":
+                    cleaner_state = pure_robot.set_state(
+                        transfer_to_cleaner,
+                        cmd[1],
+                        cleaner_state
+                    )
+
+                elif cmd[0] == "start":
+                    cleaner_state = pure_robot.start(
+                        transfer_to_cleaner,
+                        cleaner_state
+                    )
+
+                elif cmd[0] == "stop":
+                    cleaner_state = pure_robot.stop(
+                        transfer_to_cleaner,
+                        cleaner_state
+                    )
+
+                else:
+                    continue
+
+                result = RobotCommand(
+                    input_state=cleaner_state,
+                    command="RESULT"
+                )
+
+                self.out_queue.put(result)
+
+            finally:
+                self.in_queue.task_done()
+
+
+def command_to_queue(command):
+    input_queue.put(command)
+
+    result = output_queue.get()
+    output_queue.task_done()
+
+    return result
+
+
+num_workers = 5
+
+for _ in range(num_workers):
+    worker = ThreadRobo(input_queue, output_queue)
+    worker.daemon = True
+    worker.start()
